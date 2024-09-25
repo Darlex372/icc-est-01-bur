656c2af088df57198b9e1ea4f881789b8626cdf4
Darlex372 <darlex2704@gmail.com>
2024-09-25 13:05:55 -0500

    Metodo Burbuja

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Metodos metodoOrdenamiento = new Metodos();
+
+        int[] arreglo = {5, 7, 30, 12, 9};
+        int[] arregloOrdenado = metodoOrdenamiento.sortByBubble(arreglo);
+        metodoOrdenamiento.printArreglo(arregloOrdenado);
+
+        int[] arregloD = {5, 7, 30, 12, 9};
+        int[] arregloOrdenadoDes = metodoOrdenamiento.sortByBubbleDes(arregloD);
+        metodoOrdenamiento.printArregloDes(arregloOrdenadoDes);
+
+        //ORDENA LAS PALABRAS ALFABETICAMENTE DE LA CADENA DE TEXTO
+        String cadena="este es un ejemplo de texto para ordenar";
+        String[] palabras =cadena.split(" ");
+        String[] palabrasOrdenara= metodoOrdenamiento.sortByBubblePalabras(palabras);
+        metodoOrdenamiento.printArreglo(palabrasOrdenara);
     }
 }
